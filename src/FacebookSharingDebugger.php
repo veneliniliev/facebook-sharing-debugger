9e70eff6491be2d914a281302eb896d3cd06e779
Venelin Iliev <venelin@provision.bg>
2016-10-19 14:45:17 +0300

    doc block fix

--- src/FacebookSharingDebugger.php
+++ src/FacebookSharingDebugger.php
@@ -2,32 +2,31 @@
 
 namespace veneliniliev\FacebookSharingDebugger;
 
+    /*
+    * https://developers.facebook.com/docs/opengraph/using-objects
+    *
+    * Updating Objects
+    *
+    * When an action is published, or a Like button pointing to the object clicked,
+    * Facebook will 'scrape' the HTML page of the object and read the meta tags.
+    * The object scrape also occurs when:
+    *
+    *      - Every 7 days after the first scrape
+    *
+    *      - The object URL is input in the Object Debugger
+    *           http://developers.facebook.com/tools/debug
+    *
+    *      - When an app triggers a scrape using an API endpoint
+    *           This Graph API endpoint is simply a call to:
+    *
+    *           POST /?id={object-instance-id or object-url}&scrape=true
+    */
+
 /**
- * Something
+ * FacebookSharingDebugger
  *
  */
 class FacebookSharingDebugger {
-
-
-    /*
-     * https://developers.facebook.com/docs/opengraph/using-objects
-     *
-     * Updating Objects
-     *
-     * When an action is published, or a Like button pointing to the object clicked,
-     * Facebook will 'scrape' the HTML page of the object and read the meta tags.
-     * The object scrape also occurs when:
-     *
-     *      - Every 7 days after the first scrape
-     *
-     *      - The object URL is input in the Object Debugger
-     *           http://developers.facebook.com/tools/debug
-     *
-     *      - When an app triggers a scrape using an API endpoint
-     *           This Graph API endpoint is simply a call to:
-     *
-     *           POST /?id={object-instance-id or object-url}&scrape=true
-     */
 
     public static function reload($url) {
         $graph = 'https://graph.facebook.com/';
