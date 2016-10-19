69761cb3bc245d6b49c4f6ad3db14dfc7b655925
Venelin Iliev <venelin@provision.bg>
2016-10-19 14:23:22 +0300

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Facebook refresh url in sharing debugger 
+
+
+## About
+
+
+## Install
+
+
+## Usage
+
+
+### License
+
+This project is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT)
+
---
+++ src/FacebookSharingDebugger.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace veneliniliev\FacebookSharingDebugger;
+
+/**
+ * Something
+ *
+ */
+class FacebookSharingDebugger {
+
+
+    /*
+		 * https://developers.facebook.com/docs/opengraph/using-objects
+		 *
+		 * Updating Objects
+		 *
+		 * When an action is published, or a Like button pointing to the object clicked,
+		 * Facebook will 'scrape' the HTML page of the object and read the meta tags.
+		 * The object scrape also occurs when:
+		 *
+		 *      - Every 7 days after the first scrape
+		 *
+		 *      - The object URL is input in the Object Debugger
+		 *           http://developers.facebook.com/tools/debug
+		 *
+		 *      - When an app triggers a scrape using an API endpoint
+		 *           This Graph API endpoint is simply a call to:
+		 *
+		 *           POST /?id={object-instance-id or object-url}&scrape=true
+		 */
+
+    public function __construct() {
+        //
+    }
+
+    public static function reload($url) {
+        $graph = 'https://graph.facebook.com/';
+        $post = 'id=' . urlencode($url) . '&scrape=true';
+
+        return self::send_post($graph, $post);
+    }
+
+    private function send_post($url, $post) {
+        $r = curl_init();
+        curl_setopt($r, CURLOPT_URL, $url);
+        curl_setopt($r, CURLOPT_POST, 1);
+        curl_setopt($r, CURLOPT_POSTFIELDS, $post);
+        curl_setopt($r, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($r, CURLOPT_CONNECTTIMEOUT, 5);
+        $data = curl_exec($r);
+        curl_close($r);
+        return $data;
+    }
+}
+
